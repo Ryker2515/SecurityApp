b330c4a4c2a065b0824e5e97e361cceae0b35228
Yogesh <yogeshtomarr23@gmail.com>
2023-05-28 13:00:45 +0530

    Changes related to List page

--- SecurityApp/Models/ManufacturerDto.cs
+++ SecurityApp/Models/ManufacturerDto.cs
@@ -3,8 +3,9 @@
     public class ManufacturerDto
     {
         public int id { get; set; }
-        public string manufacturerName { get; set; }
+        public string cveId { get; set; }
         public string productName { get; set; }
         public string versionNumber { get; set; }
+        public DateTime published { get; set; }
     }
 }
