cb0bc2750c8ab8e8b481ab30a235b2ea48a3c538
Yogesh <yogeshtomarr23@gmail.com>
2023-07-23 17:10:49 +0530

    Changes for Details pages

--- SecurityApp/Models/ManufacturerDetailsView.cs
+++ SecurityApp/Models/ManufacturerDetailsView.cs
@@ -26,7 +26,15 @@
         public string source { get; set; }
         public string baseSeverity { get; set; }
         public decimal impactScore { get; set; }
-
+        public string accessVector { get; set; }
+        public string accessComplexity { get; set; }
+        public string confidentialityImpact { get; set; }
+        public string integrityImpact { get; set; }
+        public string availabilityImpact { get; set; }
+        public bool userInteractionRequired { get; set; }
+        public bool obtainAllPrivilege { get; set; }
+        public bool obtainUserPrivilege { get; set; }
+        public bool obtainOtherPrivilege { get; set; }
         public string value { get; set; }
     }
 }
--- SecurityApp/Models/ManufacturerDto.cs
+++ SecurityApp/Models/ManufacturerDto.cs
@@ -5,6 +5,7 @@
         public int id { get; set; }
         public int manufacturerId { get; set; }
         public string cveId { get; set; }
+        public string baseSeverity { get; set; }
         public string productName { get; set; }
         public string versionNumber { get; set; }
         public DateTime published { get; set; }
